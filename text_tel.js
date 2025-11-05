1c765dca6f7db8395f253a587265a68ba2a0536f
lilipahari84-lang <lilipahari84@gmail.com>
2025-11-05 13:55:48 -0500

    Update text_tel.js

--- text_tel.js
+++ text_tel.js
@@ -1,3 +1,3 @@
-window.defaultNumber = '+1-866-240-5403';
-window.defaultText = 'Your |%model%| Your Apple ID was recently used at APPLE STORE for $1499.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not You? Immediately call Apple Support +1-866-240-5403 to Freze it!';
+window.defaultNumber = '+1 (888) 390-1396';
+window.defaultText = 'Your |%model%| Your Apple ID was recently used at APPLE STORE for $1499.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not You? Immediately call Apple Support +1 (888) 390-1396 to Freze it!';
 
